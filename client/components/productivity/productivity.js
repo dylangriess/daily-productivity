a507609d97bf770dfc6f426df0c73231af42ac8f
dylangriess <dylangriess@gmail.com>
2023-01-17 19:05:38 -0700

    added api fetch

--- client/components/productivity/productivity.js
+++ client/components/productivity/productivity.js
@@ -4,7 +4,32 @@ const quotePrompt = "Give me a random productivity quote";
 const taskPrompt =
   "Give me a random task that enhances well-being or productivity";
 
-Promise.all([])
+Promise.all([
+  fetch("https://api.openai.com/v1/engines/davinci/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${YOUR_API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: quotePrompt,
+      max_tokens: 100,
+      temperature: 0.5,
+    }),
+  }),
+  fetch("https://api.openai.com/v1/engines/davinci/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${YOUR_API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: taskPrompt,
+      max_tokens: 100,
+      temperature: 0.5,
+    }),
+  }),
+])
   .then(([quoteRes, taskRes]) => {
     return Promise.all([quoteRes.json(), taskRes.json()]);
   })
